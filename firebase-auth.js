2092244465c321968894ab6afb198fa0b17157b9
Luberisse Karl Brad Sael Badley <karlluberisse1308@gmail.com>
2025-10-11 16:34:27 -0400

    Create firebase-auth.js

---
+++ firebase-auth.js
@@ -0,0 +1,19 @@
+// Configuration Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyBVeTFmzq0KGDnGDVw-9aad6VXyWSSUDOA",
+    authDomain: "secretvault-3039b.firebaseapp.com",
+    databaseURL: "https://secretvault-3039b-default-rtdb.firebaseio.com",
+    projectId: "secretvault-3039b",
+    storageBucket: "secretvault-3039b.firebasestorage.app",
+    messagingSenderId: "243494550983",
+    appId: "1:243494550983:web:e073cc060511c013abede2",
+    measurementId: "G-WTZ5NMZPRW"
+};
+
+// Initialisation Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+console.log('Firebase initialisé avec succès');
